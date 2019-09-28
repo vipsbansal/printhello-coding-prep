b15a966da4aeffb44125da8f42feeeab73678f61
Naveen <naveen.aggarwal@proptiger.com>
2019-09-28 14:11:42 +0530

    added all data

---
+++ problems_python/count_chocolates.py
@@ -0,0 +1,15 @@
+"""
+Print Hello is a initiative which is led by two coding enthusiasts Naveen and Vipin who want to change the world with their belief that everyone can code and that too beautifully.
+"""
+
+
+class Chocolates():
+
+    def __init__():
+        chocolates = 20
+
+
+    def find_my_choclates(self):
+        for i in range(0,20):
+            self.chocolates += i
+        return self.chocolates
