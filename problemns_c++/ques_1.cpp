75afa09dde418ba4ede51e385f38260319f1f601
Vipin Bansal <vipinb@vmware.com>
2019-10-01 11:23:18 +0530

    Comments removed

--- problemns_c++/ques_1.cpp
+++ problemns_c++/ques_1.cpp
@@ -5,11 +5,9 @@ using namespace std;
 char* get_company_name(){
     char *initial = "Print";
     char *rest_name = "hello";
-    //You cant concat the string like this. Initial can't be pointer, it should have enough space to copy second one
     return strcat(initial,rest_name);
 }
 int main() {
-//Change %%%% with %s.
     char *name = get_company_name();
     printf("%s is the best educational gateway for every year to kickstart their coding life. Thapar Alumni "
     "who gave their heart in teaching languages and prepare students for their placements now starting the best languages batches C/C++ and Python from 8th October onwards."
