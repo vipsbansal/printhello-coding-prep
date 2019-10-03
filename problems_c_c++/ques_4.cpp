9c5c0614009665d5048f18ba909cc3e33076115e
Vipin Bansal <vipinb@vmware.com>
2019-10-03 08:38:49 +0530

    Bug reemoved from ques4

--- problems_c_c++/ques_4.cpp
+++ problems_c_c++/ques_4.cpp
@@ -10,15 +10,3 @@ int main() {
     printf("Learn this amazing IEEE ranked #1 Language with us\nRegister Now on\nwww.printhello.in");
     return 0;
 }
-#include <iostream>
-using namespace std;
-int main() {
-    printf("----------------PrintHello----------------\n\n");
-    printf("Facts about PYTHON\n");
-    printf("1. Python doesn't need semi column(;)\n")
-    printf("2. They don't need brackets"{}\n");
-    printf("3. They work on indentation\n")                                                                                                                                                                                                                            \;
-    printf("4. Python helps you to make project in AI, ML, Computer Vison.\n\n");
-    printf("Learn this amazing IEEE ranked #1 Language with us\nRegister Now on\nwww.printhello.in");
-    return 0;
-}
